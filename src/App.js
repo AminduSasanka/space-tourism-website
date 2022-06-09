49b2806481a4f16ba2f3cda13b3ff4061d58d7d2
AminduSasanka <reservedchery@gmail.com>
2022-06-10 00:30:29 +0530

    technologies-page added

--- src/App.js
+++ src/App.js
@@ -2,13 +2,15 @@ import "./App.css";
 import LandingPage from "./components/landing-page/landing-page";
 import Destinations from "./components/destination-page/destinations";
 import CrewPage from "./components/crew-page/crew-page";
+import Technologies from "./components/technology-page/technologies";
 
 function App() {
   return (
     <>
       {/* <Destinations></Destinations> */}
       {/* <LandingPage></LandingPage> */}
-      <CrewPage></CrewPage>
+      {/* <CrewPage></CrewPage> */}
+      <Technologies></Technologies>
     </>
   );
 }
