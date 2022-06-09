f538b134f43d1baabb7a1a250e848e25a4c3a2ff
AminduSasanka <reservedchery@gmail.com>
2022-06-09 20:30:01 +0530

    crew page added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import "./App.css";
 import LandingPage from "./components/landing-page/landing-page";
 import Destinations from "./components/destination-page/destinations";
+import CrewPage from "./components/crew-page/crew-page";
 
 function App() {
   return (
     <>
-      <Destinations></Destinations>
+      {/* <Destinations></Destinations> */}
+      {/* <LandingPage></LandingPage> */}
+      <CrewPage></CrewPage>
     </>
   );
 }
